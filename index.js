b8a1733a71c2040437d22666d83fd0cf53e154d7
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-10 15:25:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -51,10 +51,7 @@ bot.on("message", async message => {
   .addField("Top 5", [
     `<@${sortable[0][0]}> - ${sortable [0][1]} wins`,
     `<@${sortable[1][0]}> - ${sortable [1][1]} wins`,
-    `<@${sortable[2][1]}> - ${sortable [2][2]} wins`,
-    `<@${sortable[3][2]}> - ${sortable [3][3]} wins`,
-    `<@${sortable[4][3]}> - ${sortable [4][4]} wins`,
-    `<@${sortable[5][4]}> - ${sortable [5][5]} wins`
+    `<@${sortable[2][1]}> - ${sortable [2][2]} wins`
   ].join("\n"));
   leaderboard.send(embed);
   
